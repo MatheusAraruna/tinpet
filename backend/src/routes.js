a7ab601ffa75eb55a6a9519028ea4504f93754e2
Matheus Arauna <matheusararuna90@gmail.com>
2020-12-27 18:28:46 -0300

    Add middleaware in routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,18 @@
+//Imports
 const express = require('express');
 const app = express.Router();
-
+//Controllers
 const animalController = require('./api/controllers/animalController')
 const userController = require('./api/controllers/useController')
-
+//Middlewares
+const upload = require('./api/middlewares/upload')
 //Routes animals
 app.get('/animal', animalController.list)
-app.post('/animal', animalController.create)
-app.put('/animal/:id', animalController.update)
-
-//Routes user
+app.post('/animal', upload.array('image', 4), animalController.create)
+app.put('/animal/:id', upload.array('image', 4), animalController.update)
+//Routes users
 app.get('/user', userController.list)
-app.post('/user', userController.create)
-app.put('/user:id', userController.update)
+app.post('/user', upload.single('avatar'), userController.create)
+app.put('/user/:id', upload.single('avatar'), userController.update)
 
 module.exports = app
