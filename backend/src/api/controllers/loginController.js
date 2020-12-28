492bfe19bcfb75f5ecd3fb8c77c026a17abee3f5
Matheus Arauna <matheusararuna90@gmail.com>
2020-12-28 01:32:28 -0300

    Add routes login

---
+++ backend/src/api/controllers/loginController.js
@@ -0,0 +1,7 @@
+async function login(req, res){
+    return res.json({"hey":"hey"})
+}
+async function logout(req, res){
+    return res.json({"hey":"hey"})
+}
+module.exports = { login }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const app = express.Router();
 //Controllers
 const animalController = require('./api/controllers/animalController')
 const userController = require('./api/controllers/useController')
+const loginController = require('./api/controllers/loginController')
 //Middlewares
 const upload = require('./api/middlewares/upload')
 //Routes animals
@@ -14,5 +15,7 @@ app.put('/animal/:id', upload.array('image', 4), animalController.update)
 app.get('/user', userController.list)
 app.post('/user', upload.single('image'), userController.create)
 app.put('/user/:id', upload.single('image'), userController.update)
+//Routes login/logout
+app.post('/login', loginController.login)
 
 module.exports = app
